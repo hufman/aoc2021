c2807f5e9bfe1c01156d07bc09fdb61d8cb24df6
Walter Huf <hufman@gmail.com>
2021-12-03 11:07:19 -0800

    Day 3a numeric

--- src/day3.rs
+++ src/day3.rs
@@ -1,29 +1,37 @@
 #[aoc_generator(day3)]
-pub fn input_generator(input: &str) -> Vec<String> {
+pub fn input_generator(input: &str) -> Vec<Vec<u8>> {
     input.lines()
-        .map(|line| line.to_string())
+        .map(|line| line.chars()
+            .map(|c|c.to_digit(10))
+            .flatten()
+            .map(|i| i as u8)
+            .collect())
         .collect()
 }
 
 #[aoc(day3, part1)]
-pub fn solve_part1(lines: &[String]) -> u32 {
-    // let lines: &[&str] = input.lines().collect();
-    let gamma_str: String = lines.first()
+pub fn solve_part1(data: &[Vec<u8>]) -> u32 {
+    let gamma_str: Vec<u8> = data.first()
         .unwrap()
-        .chars()
+        .iter()
         .enumerate()
-        .map(|(i, _)| if count_bits(lines, i) < lines.len()/2 { '0' } else { '1' })
+        .map(|(i, _)| if count_bits(data, i) < data.len()/2 { 0 } else { 1 })
         .collect();
-    let gamma = u32::from_str_radix(gamma_str.as_str(), 2).unwrap();
-    let epsilon_str: String = gamma_str.chars()
-        .map(|c| if c == '0' { '1' } else { '0' })
+    let gamma =  to_u32(gamma_str.as_slice());
+    let epsilon_str: Vec<u8> = gamma_str.iter()
+        .map(|c| if *c == 0 { 1 } else { 0 })
         .collect();
-    let epsilon = u32::from_str_radix(epsilon_str.as_str(), 2).unwrap();
+    let epsilon = to_u32(epsilon_str.as_slice());
     gamma * epsilon
 }
 
-fn count_bits(lines: &[String], bit: usize) -> usize {
-    lines.iter()
-        .filter(|l| l.chars().nth(bit) == Some('1'))
+fn count_bits(data: &[Vec<u8>], bit: usize) -> usize {
+    data.iter()
+        .filter(|l| l[bit] == 1)
         .count()
+}
+
+// https://www.reddit.com/r/rust/comments/36ixl0/comment/crehkpw/
+fn to_u32(slice: &[u8]) -> u32 {
+    slice.iter().fold(0, |acc, &b| acc*2 + b as u32)
 }
