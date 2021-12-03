40a3bd681b33b67744eee78600c5ad1c8d7b992f
Walter Huf <hufman@gmail.com>
2021-12-03 11:52:13 -0800

    Day 3b

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,20 @@
+pub fn test_generator() -> Vec<Vec<u8>> {
+    vec![
+        vec![0, 0, 1, 0, 0],
+        vec![1, 1, 1, 1, 0],
+        vec![1, 0, 1, 1, 0],
+        vec![1, 0, 1, 1, 1],
+        vec![1, 0, 1, 0, 1],
+        vec![0, 1, 1, 1, 1],
+        vec![0, 0, 1, 1, 1],
+        vec![1, 1, 1, 0, 0],
+        vec![1, 0, 0, 0, 0],
+        vec![1, 1, 0, 0, 1],
+        vec![0, 0, 0, 1, 0],
+        vec![0, 1, 0, 1, 0],
+    ]
+}
+
 #[aoc_generator(day3)]
 pub fn input_generator(input: &str) -> Vec<Vec<u8>> {
     input.lines()
@@ -15,7 +32,7 @@ pub fn solve_part1(data: &[Vec<u8>]) -> u32 {
         .unwrap()
         .iter()
         .enumerate()
-        .map(|(i, _)| if count_bits(data, i) < data.len()/2 { 0 } else { 1 })
+        .map(|(i, _)| most_common_bit(data, i))
         .collect();
     let gamma =  to_u32(gamma_str.as_slice());
     let epsilon_str: Vec<u8> = gamma_str.iter()
@@ -25,13 +42,62 @@ pub fn solve_part1(data: &[Vec<u8>]) -> u32 {
     gamma * epsilon
 }
 
-fn count_bits(data: &[Vec<u8>], bit: usize) -> usize {
+fn count_bits(data: &[Vec<u8>], pos: usize) -> usize {
     data.iter()
-        .filter(|l| l[bit] == 1)
+        .filter(|l| l[pos] == 1)
         .count()
+}
+
+/**
+Returns a 1 if the given column has more or the same count of 1 than 0
+else 0
+*/
+fn most_common_bit(data: &[Vec<u8>], pos: usize) -> u8 {
+    let threshold = data.len() as f32 / 2.0;
+    if (count_bits(data, pos) as f32) < threshold { 0 } else { 1 }
+}
+/**
+Returns a 1 if the given column has lower count of 1 than 0
+else 0
+ */
+fn least_common_bit(data: &[Vec<u8>], pos: usize) -> u8 {
+    let threshold = data.len() as f32 / 2.0;
+    if (count_bits(data, pos) as f32) < threshold { 1 } else { 0 }
 }
 
 // https://www.reddit.com/r/rust/comments/36ixl0/comment/crehkpw/
 fn to_u32(slice: &[u8]) -> u32 {
     slice.iter().fold(0, |acc, &b| acc*2 + b as u32)
+}
+
+
+#[aoc(day3, part2)]
+pub fn solve_part2(data: &[Vec<u8>]) -> u32 {
+    let oxygen = to_u32(filter(data, 0, true).as_slice());
+    let co2 = to_u32(filter(data, 0, false).as_slice());
+    oxygen * co2
+}
+
+fn filter(data: &[Vec<u8>], pos: usize, find_most_common: bool) -> Vec<u8> {
+    let criteria = if find_most_common {
+        most_common_bit(data, pos)
+    } else {
+        least_common_bit(data, pos)
+    };
+    let filtered: Vec<Vec<u8>> = data.iter()
+        .filter(|l| l[pos] == criteria)
+        .map(|l| l.to_owned())
+        .collect();
+    // println!("Step {} has {:?}", pos, filtered);
+    if filtered.len() == 1 {
+        filtered[0].clone()
+    } else {
+        filter(filtered.as_slice(), pos+1, find_most_common)
+    }
+}
+
+#[test]
+fn test_filter() {
+    let oxygen_str = filter(test_generator().as_slice(), 0, true);
+    assert_eq!(vec![1,0,1,1,1], oxygen_str)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,6 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
-#[macro_use]
-extern crate lazy_static;
-
 pub mod day1;
 pub mod day2;
 pub mod day3;
