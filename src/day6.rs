615d9f94a13a2f94a6e8c31c3469826383300562
Walter Huf <hufman@gmail.com>
2021-12-05 21:31:32 -0800

    Day6 smart

--- src/day6.rs
+++ src/day6.rs
@@ -14,22 +14,34 @@ pub fn input_generator(input: &str) -> Vec<Fish> {
 
 
 #[aoc(day6, part1)]
-pub fn solve_part1(starting_fish: &[Fish]) -> u32 {
-    let mut fishes: Vec<Fish> = starting_fish.to_vec();
-    for _day in 0..80 {
-        cycle(&mut fishes)
+pub fn solve_part1(starting_fish: &[Fish]) -> u64 {
+    let mut fish_buckets = [0u64; 9];
+    for fish in starting_fish.iter() {
+        fish_buckets[fish.age as usize] += 1
     }
-    fishes.len() as u32
+    for _day in 0..80 {
+        cycle(&mut fish_buckets)
+    }
+    fish_buckets.iter().sum()
 }
 
-fn cycle(fishes: &mut Vec<Fish>) {
-    let mut new_fishes: Vec<Fish> = Vec::new();
-    fishes.iter_mut().for_each(|f| {
-        if f.age == 0 {
-            f.age = 7;
-            new_fishes.push(Fish { age: 8 })
-        }
-        f.age -=1;
-    });
-    fishes.extend(new_fishes)
+#[aoc(day6, part2)]
+pub fn solve_part2(starting_fish: &[Fish]) -> u64 {
+    let mut fish_buckets = [0u64; 9];
+    for fish in starting_fish.iter() {
+        fish_buckets[fish.age as usize] += 1
+    }
+    for _day in 0..256 {
+        cycle(&mut fish_buckets)
+    }
+    fish_buckets.iter().sum()
+}
+
+fn cycle(fish_buckets: &mut [u64; 9]) {
+    let new_fishes = fish_buckets[0];
+    for time_remaining in 0..8 {
+        fish_buckets[time_remaining] = fish_buckets[time_remaining + 1]
+    }
+    fish_buckets[6] += new_fishes;
+    fish_buckets[8] = new_fishes
 }
