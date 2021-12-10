0bb24a8fed81a93cc36b68cf78cca6f9227a0a1b
Walter Huf <hufman@gmail.com>
2021-12-09 17:40:22 -0800

    Day 9b

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+use crate::day9::BasinState::{MOUNTAIN, UNEXPLORED};
 
 #[derive(Debug)]
 pub struct Point {
@@ -16,6 +18,10 @@ pub fn input_generator(input: &str) -> Vec<Vec<u8>> {
 
 #[aoc(day9, part1)]
 pub fn solve_part1(lines: &[Vec<u8>]) -> u32 {
+    lowest_points(lines).iter().map(|p| lines[p.y][p.x] as u32 + 1).sum()
+}
+
+fn lowest_points(lines: &[Vec<u8>]) -> Vec<Point> {
     let mut points: Vec<Point> = Vec::new();
     for y in 0..lines.len() {
         for x in 0..lines[y].len() {
@@ -29,5 +35,65 @@ pub fn solve_part1(lines: &[Vec<u8>]) -> u32 {
             }
         }
     }
-    points.iter().map(|p| lines[p.y][p.x] as u32 + 1).sum()
+    points
+}
+
+#[aoc(day9, part2)]
+pub fn solve_part2(lines: &[Vec<u8>]) -> u32 {
+    let seeds = lowest_points(lines);
+    let sizes: Vec<u32> = seeds.into_iter().map(|p| find_basin_size(lines, p))
+        .sorted().rev().collect();
+    return sizes[0] * sizes[1] * sizes[2]
+
+}
+
+#[derive(PartialEq)]
+enum BasinState {
+    MOUNTAIN,
+    UNEXPLORED,
+    EXPLORED
+}
+pub fn find_basin_size(lines: &[Vec<u8>], start: Point) -> u32 {
+    let mut basin_exploration: Vec<Vec<BasinState>> = Vec::new();
+
+    for y in 0..lines.len() {
+        basin_exploration.push(Vec::new());
+        for x in 0..lines[y].len() {
+            if lines[y][x] == 9 {
+                basin_exploration[y].push(MOUNTAIN);
+            } else {
+                basin_exploration[y].push(UNEXPLORED);
+            }
+        }
+    }
+
+    crawl_basin(&mut basin_exploration, start)
+}
+
+fn crawl_basin(map: &mut Vec<Vec<BasinState>>, start: Point) -> u32 {
+    let mut distance = 0u32;
+
+    if map[start.y][start.x] == BasinState::UNEXPLORED {
+        distance += 1;
+        // color this point
+        map[start.y][start.x] = BasinState::EXPLORED;
+
+        // left
+        if start.x > 0 {
+            distance += crawl_basin(map, Point { x: start.x - 1, y: start.y })
+        }
+        // up
+        if start.y > 0 {
+            distance += crawl_basin(map, Point { x: start.x, y: start.y - 1 })
+        }
+        // right
+        if start.x < map[start.y].len() - 1 {
+            distance += crawl_basin(map, Point { x: start.x + 1, y: start.y })
+        }
+        // down
+        if start.y < map.len() - 1 {
+            distance += crawl_basin(map, Point { x: start.x, y: start.y + 1 })
+        }
+    }
+    distance
 }
--- tests/day9_test.rs
+++ tests/day9_test.rs
@@ -1,7 +1,7 @@
-use aoc2021::day9::{input_generator, solve_part1};
+use aoc2021::day9::{find_basin_size, input_generator, Point, solve_part1, solve_part2};
 
 #[test]
-fn day4a_example() {
+fn day9a_example() {
     let input = "2199943210
 3987894921
 9856789892
@@ -9,4 +9,19 @@ fn day4a_example() {
 9899965678";
     let data = input_generator(input);
     assert_eq!(15, solve_part1(data.as_slice()));
+}
+
+#[test]
+fn day9b_example() {
+    let input = "2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+    let data = input_generator(input);
+    assert_eq!(3, find_basin_size(&data, Point{x: 0, y: 0}));
+    assert_eq!(9, find_basin_size(&data, Point{x: 9, y: 0}));
+    assert_eq!(14, find_basin_size(&data, Point{x: 2, y: 2}));
+    assert_eq!(9, find_basin_size(&data, Point{x: 8, y: 4}));
+    assert_eq!(1134, solve_part2(&data));
 }
