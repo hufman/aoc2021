858abc1acbc2e636b9ad5e5cf1ef94da82362311
Walter Huf <hufman@gmail.com>
2021-12-14 18:39:19 -0800

    Day 14b

--- src/day14.rs
+++ src/day14.rs
@@ -24,11 +24,16 @@ pub fn input_generator(input: &str) -> PolymerInput {
 }
 
 #[aoc(day14, part1)]
-pub fn solve_part1(input: &PolymerInput) -> u32 {
+pub fn solve_part1(input: &PolymerInput) -> u64 {
     expand_polymer_count(input, 10)
 }
 
-fn expand_polymer_count(input: &PolymerInput, rounds: usize) -> u32 {
+#[aoc(day14, part2)]
+pub fn solve_part2(input: &PolymerInput) -> u64 {
+    expand_polymer_count(input, 40)
+}
+
+fn expand_polymer_count(input: &PolymerInput, rounds: usize) -> u64 {
     let rules = input.rules.iter().map(|entry| {
         let chars: Vec<char> = entry.0.chars().collect();
         (entry.0.to_string(), vec![chars[0].to_string() + entry.1, entry.1.to_owned() + &chars[1].to_string()])
@@ -40,7 +45,7 @@ fn expand_polymer_count(input: &PolymerInput, rounds: usize) -> u32 {
     score_polymer(&pairs, input.seed.chars().nth(0).unwrap(), input.seed.chars().nth(input.seed.len()-1).unwrap())
 }
 
-fn count_pairs(seed: &String) -> HashMap<String, u32> {
+fn count_pairs(seed: &String) -> HashMap<String, u64> {
     seed.chars()
         .tuple_windows::<(_, _)>()
         .map(|chars| chars.0.to_string() + &chars.1.to_string())
@@ -50,7 +55,7 @@ fn count_pairs(seed: &String) -> HashMap<String, u32> {
         })
 }
 
-pub fn expand_polymer(pairs: HashMap<String, u32>, rules: &HashMap<String, Vec<String>>) -> HashMap<String, u32> {
+pub fn expand_polymer(pairs: HashMap<String, u64>, rules: &HashMap<String, Vec<String>>) -> HashMap<String, u64> {
     pairs.into_iter().fold(HashMap::new(), |mut output, entry| {
         rules[&entry.0].iter().for_each(|child| {
             *output.entry(child.to_string()).or_insert(0) += entry.1
@@ -59,23 +64,24 @@ pub fn expand_polymer(pairs: HashMap<String, u32>, rules: &HashMap<String, Vec<S
     })
 }
 
-fn count_polymer(pairs: &HashMap<String, u32>, first_letter: char, last_letter: char) -> HashMap<char, u32> {
-    let mut counts: HashMap<char, u32> = pairs.iter()
-        .fold(HashMap::new(), |mut map: HashMap<char, u32>, element| {
+fn count_polymer(pairs: &HashMap<String, u64>, first_letter: char, last_letter: char) -> HashMap<char, u64> {
+    let mut counts: HashMap<char, u64> = pairs.iter()
+        .fold(HashMap::new(), |mut map: HashMap<char, u64>, element| {
             let chars: Vec<char> = element.0.chars().collect();
             *map.entry(chars[0]).or_insert(0) += element.1;
             *map.entry(chars[1]).or_insert(0) += element.1;
             map
         })
         .into_iter()
-        .map(|entry| (entry.0, entry.1/2))
         .collect();
+    // double count the front and the end
     counts.entry(first_letter).and_modify(|f| *f += 1);
     counts.entry(last_letter).and_modify(|f| *f += 1);
-    counts
+    // then divide by two
+    counts.into_iter().map(|c| (c.0, c.1/2)).collect()
 }
-fn score_polymer(pairs: &HashMap<String, u32>, first_letter: char, last_letter: char) -> u32 {
+fn score_polymer(pairs: &HashMap<String, u64>, first_letter: char, last_letter: char) -> u64 {
     let counts = count_polymer(pairs, first_letter, last_letter);
-    let fields: Vec<(&char, &u32)> = counts.iter().sorted_by(|a, b| a.1.cmp(b.1)).collect();
+    let fields: Vec<(&char, &u64)> = counts.iter().sorted_by(|a, b| a.1.cmp(b.1)).collect();
     fields[fields.len() - 1].1 - fields[0].1
 }
