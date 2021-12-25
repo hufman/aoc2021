97fde8817a1a8de1847507acbb28b05f2d584b48
Walter Huf <hufman@gmail.com>
2021-12-25 12:33:43 -0800

    Day 21b

--- src/day21.rs
+++ src/day21.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::ops::{Add, Mul};
 
 #[aoc_generator(day21)]
 pub fn input_generator(input: &str) -> Vec<u16> {
@@ -27,6 +29,7 @@ impl Iterator for DeterministicDice {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
 struct PlayboardPosition {
     position: u16,
     score: u32,
@@ -63,4 +66,75 @@ fn dice_game(mut dice: DeterministicDice, starting_positions: &[u16]) -> u32 {
     let loser = positions.iter()
         .filter(|p| p.score < 1000).nth(0).unwrap();
     loser.score * dice.total_rolls
+}
+
+struct DiceWinners {
+    left: u64,
+    right: u64,
+}
+impl Add for DiceWinners {
+    type Output = DiceWinners;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        DiceWinners{left: self.left + rhs.left, right: self.right + rhs.right}
+    }
+}
+impl Mul<u16> for DiceWinners {
+    type Output = DiceWinners;
+
+    fn mul(self, rhs: u16) -> Self::Output {
+        DiceWinners{left: self.left * rhs as u64, right: self.right * rhs as u64}
+    }
+}
+impl DiceWinners {
+    fn max(self) -> u64 {
+        if self.left > self.right {
+            self.left
+        } else {
+            self.right
+        }
+    }
+}
+
+#[aoc(day21, part2)]
+pub fn solve_part2(input: &[u16]) -> u64 {
+    let positions: Vec<PlayboardPosition> = input.iter()
+        .map(|p| PlayboardPosition{position: *p, score: 0})
+        .collect();
+    let winners = simulate_universal_winners(&[positions[0], positions[1]], 0);
+    winners.max()
+}
+
+fn simulate_universal_winners(positions: &[PlayboardPosition; 2], next_player: usize) -> DiceWinners {
+    if positions[0].score >= 21 {
+        return DiceWinners{left:1, right:0}
+    }
+    if positions[1].score >= 21 {
+        return DiceWinners{left:0, right:1}
+    }
+    let other_player = 1 - next_player;
+    let rolls: Vec<u16> = (1..=3).map(|a| {
+        (1..=3).map(|b| {
+            (1..=3).map(|c| {
+                a+b+c
+            }).collect::<Vec<u16>>()
+        }).flatten().collect::<Vec<u16>>()
+    }).flatten().collect::<Vec<u16>>();
+    let roll_counts: HashMap<u16, u16> = rolls.into_iter()
+        .fold(HashMap::new(), |mut acc, moves| {
+            *acc.entry(moves).or_default() += 1; acc
+        });
+    let scores: Vec<DiceWinners> = roll_counts.iter().map(|(roll, count)| {
+        let mut new_score = positions[next_player];
+        new_score.add(*roll);
+        // println!("New score:{:?}", new_score);
+        if next_player == 0 {
+            simulate_universal_winners(&[new_score, positions[other_player]], other_player) * *count
+        } else {
+            simulate_universal_winners(&[positions[other_player], new_score], other_player) * *count
+        }
+    }).collect();
+    scores.into_iter().fold(DiceWinners{left:0, right:0}, |acc, n| {
+        acc + n
+    })
 }
